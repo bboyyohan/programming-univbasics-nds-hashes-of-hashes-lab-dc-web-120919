6f6dd905e80f8b62be43bf1b81f93c26cf35e916
stupendous-patch-9345 <yo.park93@yahoo.com>
2019-12-09 13:10:14 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -46,15 +46,19 @@ PORTION_1 = {
    sub_category: {
      label: "Phylum",
      sub_category: {
-       label: "Class",
-       label: "Order",
-       label: "Family",
-       sub_category: {
-       label: "Genus",
-       label: "Species",
-       sub_category: nil
-       }
+       label: "Class"
      }
+   },
+   label: "Order"
+ }, 
+   label: "Family",
+   sub_category: {
+     label: "Genus",
    }
-}
+ },
+   label: "Species",
+   sub_category: nil
+ }
+ }
 end
+return naming_system
